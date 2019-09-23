24478ea277c05bf15e1755a4e206400d4806a688
ZeroWangZY <756762961@qq.com>
2019-09-23 11:53:55 +0800

    delete two dim and init prod server

--- python/haikou/run.py
+++ python/haikou/run.py
@@ -1,2 +1,11 @@
 from app import app
-app.run(debug = True)
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 2 and sys.argv[1] == 'prod':
+        from gevent.pywsgi import WSGIServer
+        http_server = WSGIServer(('', 5001), app)
+        http_server.serve_forever()
+    else:
+        app.run(debug=True, host='0.0.0.0', port=5000)
+
+    
