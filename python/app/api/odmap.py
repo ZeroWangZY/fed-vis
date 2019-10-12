0b06b8e8d5dfa5e5c7342381356085663cac39ec
ZeroWangZY <756762961@qq.com>
2019-10-12 10:32:01 +0800

    cors

--- python/app/api/odmap.py
+++ python/app/api/odmap.py
@@ -1,14 +1,26 @@
 import datetime
-
-from flask import request
+from functools import wraps
+from flask import request, make_response
 
 from app import app
 from app.service.odmap_service import get_default_odmap, get_odmap
 
 from .response import gen_response
 
+def cors(func):  
+    @wraps(func)
+    def wrapper_func(*args, **kwargs):
+        r = make_response(func(*args, **kwargs))
+        r.headers['Access-Control-Allow-Origin'] = '*'
+        r.headers['Access-Control-Allow-Methods'] = 'HEAD, OPTIONS, GET, POST, DELETE, PUT'
+        allow_headers = "Referer, Accept, Origin, User-Agent, X-Requested-With, Content-Type"
+        r.headers['Access-Control-Allow-Headers'] = allow_headers
+        return r
+
+    return wrapper_func
 
 @app.route('/api/odmap/')
+@cors
 def get_odmap_api():
     params = request.args
     type_ = params.get('type')
