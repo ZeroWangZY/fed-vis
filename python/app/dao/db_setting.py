9e80fe6d6e0b1040c3aab23cfb0c46c26c9bc16b
linbingru <bingrulin@outlook.com>
2020-03-10 09:53:18 +0800

    test: 改下数据库设置

--- python/app/dao/db_setting.py
+++ python/app/dao/db_setting.py
@@ -1,10 +1,10 @@
 import pymongo
 import psycopg2
 
-pg_conn = psycopg2.connect(host='127.0.0.1',
+pg_conn = psycopg2.connect(host='10.76.0.163',
                            database='haikou',
                            user='postgres',
-                           password='12345678')
+                           password='ni99woba')
 pg_cur = pg_conn.cursor()
 
-mongo_client = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
+mongo_client = pymongo.MongoClient("mongodb://10.76.0.163:27017/")
